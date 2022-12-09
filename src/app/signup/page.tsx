89f26f1569cc6a0f33f5d0cf3e6429714c35b528
Daniel Caldera <dan@wait3.com>
2022-12-09 03:14:57 -0600

    Sign Up Page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { toast, Toaster } from 'react-hot-toast'
 
-export default function LoginPage() {
+export default function SignupPage() {
+  const _handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    toast('Sign up is not implemented yet', { icon: '🚧' })
+  }
   return (
     <div className='flex min-h-screen'>
+      <Toaster />
       <div className='flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
         <div className='mx-auto w-full max-w-sm lg:w-96'>
           <div>
@@ -16,15 +24,30 @@ export default function LoginPage() {
                 alt='Your Company'
               />
             </Link>
-            <h2 className='mt-6 text-3xl font-bold tracking-tight text-gray-900'>Sign in to your account</h2>
+            <h2 className='mt-6 text-3xl font-bold tracking-tight text-gray-900'>Sign up to your account</h2>
           </div>
 
           <div className='mt-8'>
             <div className='mt-6'>
-              <form action='#' method='POST' className='space-y-6'>
+              <form onSubmit={_handleSubmit} method='POST' className='space-y-6'>
+                <div>
+                  <label htmlFor='username' className='block text-sm font-medium text-gray-700'>
+                    Username
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      id='username'
+                      name='username'
+                      type='text'
+                      required
+                      className='block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                    />
+                  </div>
+                </div>
+
                 <div>
                   <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
-                    Email address or Username
+                    Email address
                   </label>
                   <div className='mt-1'>
                     <input
@@ -54,39 +77,21 @@ export default function LoginPage() {
                   </div>
                 </div>
 
-                <div className='flex items-center justify-between'>
-                  <div className='flex items-center'>
-                    <input
-                      id='remember-me'
-                      name='remember-me'
-                      type='checkbox'
-                      className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                    />
-                    <label htmlFor='remember-me' className='ml-2 block text-sm text-gray-900'>
-                      Remember me
-                    </label>
-                  </div>
-
-                  <div className='text-sm'>
-                    <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                      Forgot your password?
-                    </a>
-                  </div>
-                </div>
+                <div className='flex items-center justify-between'></div>
 
                 <div>
                   <button
                     type='submit'
                     className='flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                   >
-                    Sign in
+                    Sign up
                   </button>
                 </div>
 
                 <div className='flex items-center justify-center'>
                   <div className='text-sm'>
-                    <Link href='/signup' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                      Create an account
+                    <Link href='/login' className='font-medium text-indigo-600 hover:text-indigo-500'>
+                      Already have an account? Sign in
                     </Link>
                   </div>
                 </div>
